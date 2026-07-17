31187b89690d165dff4323e7afa7d9986dca04c7
ankit-8-8 <ankit449513@gmail.com>
2026-07-17 12:59:41 +0530

    fix url add to cart in category page

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,5 @@
-export const apiBaseUrl =
-  "https://vyraa-backend-production.up.railway.app/api/";
+export const apiBaseUrl = "http://localhost:7777/api"
+// "https://vyraa-backend-production.up.railway.app/api/";
 
 export const apiUrls = {
   // Category
