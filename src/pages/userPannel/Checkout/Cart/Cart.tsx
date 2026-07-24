34e82de5e3ddd3b8a8a9b811295a352a1f015520
ankit-8-8 <ankit449513@gmail.com>
2026-07-24 17:03:34 +0530

    update orderDetails page for user

--- src/pages/userPannel/Checkout/Cart/Cart.tsx
+++ src/pages/userPannel/Checkout/Cart/Cart.tsx
@@ -46,6 +46,7 @@ const Cart = () => {
   const refreshCart = () => setRefreshKey((prev) => prev + 1);
 
   useEffect(() => {
+    window.scrollTo(0, 0)
     getQuery({
       url: apiUrls.Cart.getByUserId,
       onSuccess: (res: any) => {
@@ -66,7 +67,7 @@ const Cart = () => {
             const currentSizeId = apiItem.size?._id || apiItem.size;
             const sizeData = priceList.find(
               (p: any) => String(p.size?._id || p.size) === String(currentSizeId)
-            ) || priceList[0] || {}; 
+            ) || priceList[0] || {};
 
             // Extract exact prices based on the matched size data
             const baseMrp = sizeData.markupPrice || apiItem.unitPrice || 0;
